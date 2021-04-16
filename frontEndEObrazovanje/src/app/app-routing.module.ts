ab21393c67ace5f7bead0de01ebf1bb668f9bf77
rakin99 <rakindejan@gmail.com>
2021-04-16 11:58:38 +0200

    rutiranje za account

--- frontEndEObrazovanje/src/app/app-routing.module.ts
+++ frontEndEObrazovanje/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './components/account/account.component';
 import { ExamDetailComponent } from './components/exam-detail/exam-detail.component';
 import { ExamsComponent } from './components/exams/exams.component';
 import { LoginComponent } from './components/login/login.component';
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'exams', component: ExamsComponent },
   { path: 'exam-detail', component: ExamDetailComponent },
+  { path: 'account', component: AccountComponent },
   { path: '**', redirectTo: '' }
 ];
 
