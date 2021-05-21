c6f36a38da581508b026a0ec19ba2c1d8e3784ba
Svetozar99 <svetozarbrboric@gmail.com>
2021-05-21 14:01:43 +0200

    prikaz paymenta za studenta

--- frontEndEObrazovanje/src/app/components/payment/payment.component.ts
+++ frontEndEObrazovanje/src/app/components/payment/payment.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Payment } from 'src/app/model/payment';
+import { Student } from 'src/app/model/student';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { StudentDetailComponent } from '../student-detail/student-detail.component';
 import { PaymentService } from './payment.service';
 
 @Component({
@@ -15,15 +18,30 @@ export class PaymentComponent implements OnInit {
 
   subscription: Subscription;
 
+  @Input() student:Student = new Student({
+    id:0,
+    cardNumber: '',
+    userDTO:{
+      id:0,
+      firstName:'',
+      lastName:'',
+      userName:'',
+      password:'',
+      roles:[]
+    }
+  })
 
-  constructor(private paymentService: PaymentService, private router: Router) {
+  constructor(private paymentService: PaymentService, private auths: AuthenticationService ,private router: Router,private studentDetail: StudentDetailComponent) {
+    if(auths.getRole() === 'ROLE_ADMINISTRATOR'){
+      this.student = studentDetail.student;
+    }
     this.subscription = paymentService.RegenerateData$.subscribe(() =>
-      this.getPayments()
-    );
+      // this.getPayments()
+      this.getStudentPayments());
   }
 
   ngOnInit(): void {
-    this.getPayments();
+    this.getStudentPayments();
   }
 
   getPayments(){
@@ -32,5 +50,19 @@ export class PaymentComponent implements OnInit {
         this.payments = response.body;
       }
     )
+  }
+
+  getStudentPayments(){
+    this.paymentService.getStudentPayments(this.student.userDTO.userName).subscribe(
+      response => {
+        this.payments = response.body;
+      }
+    )
+  }
+
+  dateToString(date:Date):Date{
+    var d = new Date(date);
+    d.setHours(d.getHours()-1);
+    return d;
   }
 }
