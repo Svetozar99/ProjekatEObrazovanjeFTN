67796fb9ac663101b6ebfea23253a1895a67ed23
Svetozar99 <svetozarbrboric@gmail.com>
2021-05-30 22:10:24 +0200

    dodao role-guard

--- frontEndEObrazovanje/src/app/guards/role-guard.ts
+++ frontEndEObrazovanje/src/app/guards/role-guard.ts
@@ -47,6 +47,60 @@ export class RoleGuard implements CanActivate{
 		}else if(info.roles[0].authority.indexOf("ROLE_ADMINISTRATOR") === -1 && route.url.toString() === "view-course-specification/:id"){
 			this.router.navigate(['/home']);
 			return false;
+		}else if(info.roles[0].authority.indexOf("ROLE_ADMINISTRATOR") === -1 && info.roles[0].authority.indexOf("ROLE_TEACHER") && route.url.toString() === "course-instance/:id"){
+			this.router.navigate(['/home']);
+			return false;
+		}else if(info.roles[0].authority.indexOf("ROLE_ADMINISTRATOR") === -1 && route.url.toString() === "add-user"){
+			this.router.navigate(['/home']);
+			return false;
+		}else if(info.roles[0].authority.indexOf("ROLE_ADMINISTRATOR") === -1 && route.url.toString() === "students"){
+			this.router.navigate(['/home']);
+			return false;
+		}else if(info.roles[0].authority.indexOf("ROLE_ADMINISTRATOR") === -1 && route.url.toString() === "teachers"){
+			this.router.navigate(['/home']);
+			return false;
+		}else if(info.roles[0].authority.indexOf("ROLE_ADMINISTRATOR") === -1 && route.url.toString() === "teacher/:id"){
+			this.router.navigate(['/home']);
+			return false;
+		}else if(info.roles[0].authority.indexOf("ROLE_ADMINISTRATOR") === -1 && route.url.toString() === "student-detail/:id"){
+			this.router.navigate(['/home']);
+			return false;
+		}else if(info.roles[0].authority.indexOf("ROLE_ADMINISTRATOR") === -1 && route.url.toString() === "document/add-for/:username"){
+			this.router.navigate(['/home']);
+			return false;
+		}else if(info.roles[0].authority.indexOf("ROLE_ADMINISTRATOR") === -1 && info.roles[0].authority.indexOf("ROLE_TEACHER") && route.url.toString() === "student-exam-detail/:id/:cardNumber"){
+			this.router.navigate(['/home']);
+			return false;
+		}else if(info.roles[0].authority.indexOf("ROLE_ADMINISTRATOR") === -1 && route.url.toString() === "view-user/:id"){
+			this.router.navigate(['/home']);
+			return false;
+		}else if(info.roles[0].authority.indexOf("ROLE_ADMINISTRATOR") === -1 && info.roles[0].authority.indexOf("ROLE_TEACHER") && route.url.toString() === "course-instance/exam-parts/:id"){
+			this.router.navigate(['/home']);
+			return false;
+		}else if(info.roles[0].authority.indexOf("ROLE_TEACHER") === -1 && route.url.toString() === "teacher-exam-parts/:id"){
+			this.router.navigate(['/home']);
+			return false;
+		}else if(info.roles[0].authority.indexOf("ROLE_TEACHER") === -1 && route.url.toString() === "add-exam-part"){
+			this.router.navigate(['/home']);
+			return false;
+		}else if(info.roles[0].authority.indexOf("ROLE_STUDENT") === -1 && route.url.toString() === "exams"){
+			this.router.navigate(['/home']);
+			return false;
+		}else if(info.roles[0].authority.indexOf("ROLE_STUDENT") === -1 && route.url.toString() === "exam-detail/student/:id"){
+			this.router.navigate(['/home']);
+			return false;
+		}else if(info.roles[0].authority.indexOf("ROLE_STUDENT") === -1 && route.url.toString() === "account"){
+			this.router.navigate(['/home']);
+			return false;
+		}else if(info.roles[0].authority.indexOf("ROLE_STUDENT") === -1 && route.url.toString() === "add-payment"){
+			this.router.navigate(['/home']);
+			return false;
+		}else if(info.roles[0].authority.indexOf("ROLE_STUDENT") === -1 && route.url.toString() === "documents"){
+			this.router.navigate(['/home']);
+			return false;
+		}else if(info.roles[0].authority.indexOf("ROLE_STUDENT") === -1 && route.url.toString() === "document"){
+			this.router.navigate(['/home']);
+			return false;
 		}
 		return true;
     }
